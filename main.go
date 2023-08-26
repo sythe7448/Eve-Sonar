eced214ffa5b09c581bb74248d9f6f7b6fd3a175
jakec <jakecolburn.sythe@gmail.com>
2023-08-26 00:29:23 -0500

    chore: changed theme to darkmode

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/theme"
 	"stagingRangeWarning/eveSolarSystems"
 )
 
 func main() {
 	trackerApp := app.New()
+	trackerApp.Settings().SetTheme(theme.DarkTheme())
 	trackerWindow := trackerApp.NewWindow("Eve Staging Range Tracker")
 
 	appContainer := eveSolarSystems.BuildContainer(trackerApp)
